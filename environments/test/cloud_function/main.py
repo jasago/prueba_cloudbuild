aa656e931863d49fd297f3e5736b57af2e14b7b5
jasago <jasago0408@gmail.com>
2022-04-12 13:07:53 -0600

    prueba function 6

--- environments/test/cloud_function/main.py
+++ environments/test/cloud_function/main.py
@@ -2,12 +2,12 @@ from __future__ import print_function
 from google.cloud import vision
 
 def emo_detect():
-    #uri_base = 'gs://project1-test-346303-input'
     
-    #pics = ('image_to_analyze.jpg')
+    uri_base = 'gs://project1-test-346303-input'
+    pic = ('image_to_analyze.jpg')
     
-    uri_base = 'gs://cloud-vision-codelab'
-    pic = 'face_surprise.jpg'
+    #uri_base = 'gs://cloud-vision-codelab'
+    #pic = 'face_surprise.jpg'
 
 
     client = vision.ImageAnnotatorClient()
@@ -17,8 +17,7 @@ def emo_detect():
     image.source.image_uri = '%s/%s' % (uri_base, pic)
     response = client.face_detection(image=image)
 
-        # print('=' * 30)
-        # print('File:', pic)
+        
     for face in response.face_annotations:
         print("entro 2")
         print(face.anger_likelihood, "    ", face.joy_likelihood, "   ", face.surprise_likelihood, "     ", face.sorrow_likelihood)
