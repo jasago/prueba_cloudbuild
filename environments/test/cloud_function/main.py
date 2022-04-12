b1758ef17e6d483602d3c432af5ca82115aeeac7
jasago <jasago0408@gmail.com>
2022-04-12 12:47:25 -0600

    prueba function 4

--- environments/test/cloud_function/main.py
+++ environments/test/cloud_function/main.py
@@ -12,19 +12,18 @@ def emo_detect():
     image = vision.Image()
 
     for pic in pics:
+        print("entro")
         image.source.image_uri = '%s/%s' % (uri_base, pic)
         response = client.face_detection(image=image)
 
         # print('=' * 30)
         # print('File:', pic)
         for face in response.face_annotations:
+            print("entro 2")
             print(face.anger_likelihood, "    ", face.joy_likelihood, "   ",
               face.surprise_likelihood, "     ", face.sorrow_likelihood)
-            # likelihood = vision.Likelihood(face.surprise_likelihood)
-            # vertices = ['(%s,%s)' % (v.x, v.y) for v in face.bounding_poly.vertices]
-            # print('Face surprised:', likelihood.name)
-            # print('Face bounds:', ",".join(vertices))
-
+            print("paso")
+    print("termino")
 def main(event , context):
     print("image num       anger       joy      surprice     sorrow")
     emo_detect()
