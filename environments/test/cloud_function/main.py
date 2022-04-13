e1583eb260819248047d7a1c829c8716ddc826ae
jasago <jasago0408@gmail.com>
2022-04-12 19:24:15 -0600

    pruebas pyton2

--- environments/test/cloud_function/main.py
+++ environments/test/cloud_function/main.py
@@ -17,7 +17,7 @@ def emo_detect(uri_base, pic):
         print("Anger: " + str(face.anger_likelihood), " Joy: "+ str(face.joy_likelihood), " Surprice: " +
          str(face.surprise_likelihood), " Sorrow: "+ str(face.sorrow_likelihood))
     
-    return True
+    #return True
     
 def main(event , context):
     uri_base = 'gs://project1-test-346303-input' #Bucket donde esta la imagen
