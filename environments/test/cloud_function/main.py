9cc71519c9a6fdc9b7093bf0be1ff26142daf818
jasago <jasago0408@gmail.com>
2022-04-13 01:40:37 -0600

    usando lint pylint 3

--- environments/test/cloud_function/main.py
+++ environments/test/cloud_function/main.py
@@ -3,17 +3,15 @@ from google.cloud import vision
 
 
 def emo_detect(uri_base, pic):
- 
     client = vision.ImageAnnotatorClient()
-    image = vision.Image()
-           
+    image = vision.Image()   
     image.source.image_uri = '%s/%s' % (uri_base, pic)
     response = client.face_detection(image=image)
-
     faces = response.face_annotations
         
     for face in faces:
-        print("Anger: " + str(face.anger_likelihood), " Joy: "+ str(face.joy_likelihood), " Surprice: " +
+        print("Anger: " + str(face.anger_likelihood),
+         " Joy: "+ str(face.joy_likelihood), " Surprice: " +
          str(face.surprise_likelihood), " Sorrow: "+ str(face.sorrow_likelihood))
     return True
     
