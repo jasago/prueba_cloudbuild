fafb6b048e943fdf5d692a00b91fb73c7a1edc37
jasago <jasago0408@gmail.com>
2022-04-13 01:54:10 -0600

    usando lint pylint 5

--- environments/test/cloud_function/main.py
+++ environments/test/cloud_function/main.py
@@ -1,12 +1,9 @@
 from __future__ import print_function
 from google.cloud import vision
 
-"""Module providingFunction printing python version."""
 def emo_detect(uri_base, pic):
-    
     client = vision.ImageAnnotatorClient()
     image = vision.Image()
-
     image.source.image_uri = '%s/%s' % (uri_base, pic)
     response = client.face_detection(image=image)
     faces = response.face_annotations
@@ -16,8 +13,7 @@ def emo_detect(uri_base, pic):
          " Joy: "+ str(face.joy_likelihood), " Surprice: " +
          str(face.surprise_likelihood), " Sorrow: "+ str(face.sorrow_likelihood))
     return True
-    
-"""Module providingFunction printing python version."""    
+  
 def main(event , context):
     uri_base = 'gs://project1-test-346303-input' #Bucket donde esta la imagen
     pic = ('image_to_analyze.jpg') #Imagen a analizar
