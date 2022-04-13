e08287703b53d3fbc2fcb817ce7fd6da5cd29780
jasago <jasago0408@gmail.com>
2022-04-12 23:42:20 -0600

    pruebas unit pytest final

--- environments/test/cloud_function/test_function.py
+++ environments/test/cloud_function/test_function.py
@@ -2,9 +2,12 @@ import main
 
 def test_emo_detect():
     
+    #public data
     test_uri_base = 'gs://cloud-vision-codelab'
     test_pic = 'face_surprise.jpg'
 
     result = main.emo_detect(test_uri_base, test_pic)
 
     assert result is True
+
+
