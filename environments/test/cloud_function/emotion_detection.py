612e1b786972fc172351625d20778f8d8ea90161
jasago <jasago0408@gmail.com>
2022-04-14 10:22:04 -0600

    prueba main 2

--- environments/test/cloud_function/emotion_detection.py
+++ environments/test/cloud_function/emotion_detection.py
@@ -1,6 +1,12 @@
 from __future__ import print_function
 from google.cloud import vision
 
+def get_most_likely(face):
+    anger = str(face.anger_likelihood)
+    joy = str(face.joy_likelihood)
+    suprice = str(face.surprise_likelihood)
+    return True
+
 def emo_detect(uri_base, pic):
 
     client = vision.ImageAnnotatorClient()
@@ -8,12 +14,11 @@ def emo_detect(uri_base, pic):
     image.source.image_uri = 'gs://%s/%s' % (uri_base, pic)
 
     response = client.face_detection(image=image)# pylint: disable=no-member
-    faces = response.face_annotations
+    face = response.face_annotations[0]
 
-    print(type(faces))
-    print(faces[0])
+    return get_most_likely(face)
+
     # for face in faces:
     #     print("Anger: " + str(face.anger_likelihood),
     #      " Joy: "+ str(face.joy_likelihood), " Surprice: " +
     #      str(face.surprise_likelihood), " Sorrow: "+ str(face.sorrow_likelihood))
-    return True
